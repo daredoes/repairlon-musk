27441f9ca8559be8b9cb44bb7919f9f877ece4db
Arthur Z. Baney <arthur.baney@leiainc.com>
2020-02-01 15:31:29 -0800

    Create flamethrower script

---
+++ Assets/FlameThrower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameThrower : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger))
+        {
+            particleSystem.Play();
+        }
+        else
+        {
+            particleSystem.Stop();
+        }
+    }
+}
