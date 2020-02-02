88432d46480211a773a4021657367c52680ae0c3
Arthur Z. Baney <arthur.baney@leiainc.com>
2020-02-02 14:32:16 -0800

    Changes

---
+++ Assets/FlameColliderCube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameColliderCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("colliding with "+collision.gameObject.name);
+
+
+        if (gameObject.activeSelf)
+        if (collision.gameObject.name.Contains("Hole"))
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
