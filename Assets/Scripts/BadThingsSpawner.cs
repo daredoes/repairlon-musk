448408b6a4cec020f07491850212cbf4ac7fc682
Arthur Z. Baney <arthur.baney@leiainc.com>
2020-02-02 09:35:11 -0800

    Created bad things spawner prefab and script

---
+++ Assets/Scripts/BadThingsSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadThingsSpawner : MonoBehaviour
+{
+    public Transform[] badThings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartTimer(15f);
+    }
+    
+    void StartTimer(float waitTime)
+    {
+        IEnumerator timer = Timer(waitTime);
+        StartCoroutine(timer);
+    }
+
+    IEnumerator Timer(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        //Do something
+        StartTimer(15f);
+    }
+
+    void SpawnBadThing()
+    {
+
+    }
+
+
+    
+}
