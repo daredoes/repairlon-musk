1223b4ae3a32d0306705d7c3f0f334af524a00c0
Arthur Z. Baney <arthur.baney@leiainc.com>
2020-02-02 16:09:00 -0800

    Made starship icon move

---
+++ Assets/MoveShipIcon.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShipIcon : MonoBehaviour
+{
+    public GameObject landOnMarsButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.localPosition.x < .007f)
+        {
+        transform.localPosition += new Vector3(.00005f,0,0) * Time.deltaTime;
+            
+        if (transform.localPosition.x >= .007f)
+            {
+                landOnMarsButton.SetActive(true);
+            }
+        }
+    }
+}
