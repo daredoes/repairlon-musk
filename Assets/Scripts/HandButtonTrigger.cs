22ba444f8d695f4b4560203496d1b81d4bb029c8
Arthur Z. Baney <arthur.baney@leiainc.com>
2020-02-06 06:30:10 -0800

    Moved scripts to scripts folder

---
+++ Assets/Scripts/HandButtonTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HandButtonTrigger : MonoBehaviour
+{
+    bool hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<HandButton>().Press();
+        
+        //if (!hit)
+        //{
+
+        //}
+        //    if (other.name == "Land On Mars Button")
+        //{
+        //    hit = true;
+        //    Debug.Log("COLLIDING WITH R CUBE!!!");
+        //    SceneManager.LoadSceneAsync("WinScreen");
+        //}
+    }
+}
