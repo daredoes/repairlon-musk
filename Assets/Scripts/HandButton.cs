547671277520d0b7ae7c0654d0b9452d4fd348e2
Arthur Z. Baney <arthur.baney@leiainc.com>
2020-02-06 06:33:18 -0800

    Create HandButton script

---
+++ Assets/Scripts/HandButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HandButton : MonoBehaviour
+{
+    public UnityEvent action;
+    
+    public void Press()
+    {
+        action.Invoke();
+    }
+}
