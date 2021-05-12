11ccf6c687e3d7b1d71ecfa4d59f623408c5dea9
Arthur Z. Baney <azbaney@gmail.com>
2021-05-11 22:07:30 -0700

    Uncommitted changes

--- Assets/HoleInWall.cs
+++ Assets/HoleInWall.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class HoleInWall : MonoBehaviour
 {
     MeshFilter meshFilter;
+    
+    [SerializeField] private Transform flameThrowerColiderBox;
+    
+    Vector3[] startVertices, vertices;
 
+    public Transform closestPointIndicator, impactPoint;
+
+    public MeshFilter openMesh, closedMesh;
+    
     // Start is called before the first frame update
     void Start()
     {
         meshFilter = GetComponent<MeshFilter>();
 
-        Vector3[] vertices = meshFilter.mesh.vertices;
+        startVertices = meshFilter.mesh.vertices;
 
+        vertices = meshFilter.mesh.vertices;
+        
+        Impact();
+    }
+
+    void Randomize()
+    {
+        
         int count = vertices.Length;
 
         for(int i=0;i<count;i++)
@@ -24,13 +40,70 @@ public class HoleInWall : MonoBehaviour
                 Random.value * .001f
                 );
         }
+        
         meshFilter.mesh.vertices = vertices;
+        meshFilter.mesh.RecalculateNormals();
+    }
+
+
+
+    void Impact()
+    {
+        int count = vertices.Length;
+
+        for(int i=0;i<count;i++)
+        {
+            float pointDistance = Vector3.Distance(
+                GetVertexWorldPosition(vertices[i], transform), 
+                impactPoint.position
+                );
+
+            vertices[i] += new Vector3(
+                (GetVertexWorldPosition(vertices[i], transform).x - impactPoint.position.x) / 10f, 
+                0, 
+                .1f/pointDistance*Time.deltaTime
+                ); // (GetVertexWorldPosition(vertices[i], transform) - impactPoint.position) / 2f * .01f/pointDistance; //
+        }
+        
         meshFilter.mesh.RecalculateNormals();
     }
 
     // Update is called once per frame
     void Update()
     {
+        int count = startVertices.Length;
+
+        int closestPointIndex = 0;
+        float closestPointDistance = Vector3.Distance(startVertices[0], flameThrowerColiderBox.position);
+
+        for( int i=0 ; i < count ; i++ )
+        {
+            float pointDistance = Vector3.Distance(
+                GetVertexWorldPosition(vertices[i], transform), 
+                flameThrowerColiderBox.position
+                );
+
+            if ( pointDistance < closestPointDistance )
+            {
+                closestPointDistance = pointDistance;
+                closestPointIndex = i;
+            }
+            
+            Debug.Log("pointDistance = "+pointDistance);
+            
+            if (pointDistance < .1f)
+            {
+                vertices[i] += new Vector3(0, 0, -.001f/pointDistance*Time.deltaTime);
+            }
+        }
+
+        closestPointIndicator.position = GetVertexWorldPosition(vertices[closestPointIndex], transform);
         
+        meshFilter.mesh.vertices = vertices;
+    }
+
+    public Vector3 GetVertexWorldPosition(Vector3 vertex, Transform owner)
+    {
+        return owner.localToWorldMatrix.MultiplyPoint3x4(vertex);
     }
 }
---
+++ Assets/HoleInterpolater.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleInterpolater : MonoBehaviour
+{
+    public MeshFilter meshFilter, openMesh, closedMesh;
+
+    Vector3[] vertices, startVertices, targetVertices;
+    
+    [SerializeField] private Transform flameThrowerColliderBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+
+        vertices = meshFilter.mesh.vertices;
+        
+        targetVertices = closedMesh.sharedMesh.vertices;
+
+        startVertices = openMesh.sharedMesh.vertices;
+        
+        int count = vertices.Length;
+
+        for(int i=0;i<count;i++)
+        {
+            vertices[i] = startVertices[i];
+        }
+        
+        meshFilter.mesh.vertices = vertices;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int count = vertices.Length;
+
+        for(int i=0;i<count;i++)
+        {
+            if (Vector3.Distance(flameThrowerColliderBox.position, vertices[i]) < .1f)
+            {
+                vertices[i] = Vector3.Lerp(vertices[i], targetVertices[i], Time.deltaTime);
+            }
+        }
+        meshFilter.mesh.vertices = vertices;
+    }
+}
